3316a2c59797149fb839c44b84d9ab72848520ae
Marcial Bernal <marcialb9328@gmail.com>
2025-09-23 19:41:13 -0700

    Modificado modelo

--- backend/app/notes/models.py
+++ backend/app/notes/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 class Note (models.Model):
     
-    nombre = models.CharField(max_length=20)
-    telefono = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=20, blank=True, null=True)
+    telefono = models.CharField(max_length=10, blank=True, null=True)
     empresa = models.CharField(max_length=20, blank=True, null=True)
     fecha = models.DateField()
     ubicacion = models.CharField(max_length=50, blank=True, null=True)
     equipo = models.CharField(max_length=20, blank=True, null=True)
     operador = models.CharField(max_length=20, blank=True, null=True)
     ayudante = models.CharField(max_length=20, blank=True, null=True)
-    trabajo_realizar = models.TextField(max_length=100)
+    trabajo_realizar = models.TextField(max_length=100, blank=True, null=True)
     
     # Tiempos
-    salida = models.TimeField()
-    llegada = models.TimeField()
-    termino = models.TimeField()
-    retorno = models.TimeField()
+    salida = models.TimeField(blank=True, null=True)
+    llegada = models.TimeField(blank=True, null=True)
+    termino = models.TimeField(blank=True, null=True)
+    retorno = models.TimeField(blank=True, null=True)
     total_horas = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     # Costos
@@ -26,7 +26,7 @@ class Note (models.Model):
     costo_total_iva = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     # Firma cliente (puede ser imagen en base64 o archivo)
-    firma_cliente = models.ImageField(upload_to="firmas/")
+    firma_cliente = models.ImageField(max_length=20, blank=True, null=True)
 
     creado_en = models.DateTimeField(auto_now_add=True)
     actualizado_en = models.DateTimeField(auto_now=True)
